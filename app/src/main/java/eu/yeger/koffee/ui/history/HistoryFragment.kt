86591e4dfa212586bcc813b698a60611b8e3379a
Jan Müller <janmueller3698@gmail.com>
2020-06-30 14:05:49 +0200

    Added navigation to history

--- app/src/main/java/eu/yeger/koffee/ui/history/HistoryFragment.kt
+++ app/src/main/java/eu/yeger/koffee/ui/history/HistoryFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import eu.yeger.koffee.databinding.FragmentHistoryBinding
+import eu.yeger.koffee.domain.Transaction
 import eu.yeger.koffee.repository.TransactionRepository
+import eu.yeger.koffee.ui.OnClickListener
 import eu.yeger.koffee.ui.adapter.transactionListAdapter
 import eu.yeger.koffee.utility.getUserIdFromSharedPreferences
 import eu.yeger.koffee.utility.viewModelFactories
@@ -33,7 +36,15 @@ class HistoryFragment : Fragment() {
 
         return FragmentHistoryBinding.inflate(inflater).apply {
             historyViewModel = this@HistoryFragment.historyViewModel
-            transactionRecyclerView.adapter = transactionListAdapter()
+            transactionRecyclerView.adapter =
+                transactionListAdapter(OnClickListener { transaction ->
+                    val direction = when (transaction) {
+                        is Transaction.Purchase -> HistoryFragmentDirections.toItemDetails(transaction.itemId)
+                        is Transaction.Refund -> HistoryFragmentDirections.toItemDetails(transaction.itemId)
+                        is Transaction.Funding -> HistoryFragmentDirections.toHome()
+                    }
+                    findNavController().navigate(direction)
+                })
             lifecycleOwner = viewLifecycleOwner
         }.root
     }
