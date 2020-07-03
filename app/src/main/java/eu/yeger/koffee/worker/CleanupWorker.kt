8af05a07b8dcd14250fd160e2855a9f307d628e9
Jan Müller <janmueller3698@gmail.com>
2020-07-03 14:50:54 +0200

    Documented worker

--- app/src/main/java/eu/yeger/koffee/worker/CleanupWorker.kt
+++ app/src/main/java/eu/yeger/koffee/worker/CleanupWorker.kt
@@ -9,14 +9,32 @@ import eu.yeger.koffee.repository.TransactionRepository
 import eu.yeger.koffee.repository.UserRepository
 import eu.yeger.koffee.utility.getUserIdFromSharedPreferences
 
-class CleanupWorker(private val appContext: Context, params: WorkerParameters) :
-    CoroutineWorker(appContext, params) {
+/**
+ * Cleanup [Worker](https://developer.android.com/reference/androidx/work/Worker) that removed unused data from the local storage.
+ *
+ * @property appContext Context used for creating repositories.
+ * @constructor
+ * Used by [WorkManager](https://developer.android.com/jetpack/androidx/releases/work) to create instances.
+ *
+ * @param params Worker parameters. Not used by this worker.
+ *
+ * @author Jan Müller
+ */
+class CleanupWorker(
+    private val appContext: Context,
+    params: WorkerParameters
+) : CoroutineWorker(appContext, params) {
 
     private val itemRepository = ItemRepository(appContext)
     private val profileImageRepository = ProfileImageRepository(appContext)
     private val transactionRepository = TransactionRepository(appContext)
     private val userRepository = UserRepository(appContext)
 
+    /**
+     * Refreshed local data and removes data not required by the active user.
+     *
+     * @return The result of this worker.
+     */
     override suspend fun doWork(): Result {
         val activeUserId = appContext.getUserIdFromSharedPreferences()
         itemRepository.fetchItems()
